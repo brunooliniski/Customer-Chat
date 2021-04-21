215a16a78359341c988a38ac5525767e7f7a058a
Bruno <bruno.oliniski@gmail.com>
2021-04-21 10:33:36 -0300

    Depencendies
    typescript/ts-node-dev/@types-express

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+
+const app = express();
+
+/**
+ * GET = Search
+ * POST = Creation
+ * PUT = Update
+ * PATCH = Change specific info
+ * DELETE
+ */
+
+app.get("/", (request, response ) => {
+    return response.json({
+        message: "Olá"
+    })
+})
+
+app.post("/", (request, response) => {
+    return response.json({
+        message: "Usuário salvo com sucesso"
+    })
+})
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
