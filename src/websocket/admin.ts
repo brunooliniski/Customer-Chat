9ea4484b04945dcc2c34ff24bcb930a59f15096b
Bruno <bruno.oliniski@gmail.com>
2021-04-29 19:45:19 -0300

    Admin <Message> Client

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -17,4 +17,33 @@ io.on("connect", async (socket) => {
 
         callback(allMessages);
     });
+
+    socket.on("admin_send_message", async (params) => {
+        const { user_id, text } = params;
+
+        await messagesService.create({
+            text,
+            user_id,
+            admin_id: socket.id
+        })
+
+        const {socket_id} = await connectionService.findByUserId(user_id);
+
+        io.to(socket_id).emit("admin_send_to_client", {
+            text,
+            socket_id: socket.id
+        })
+    });
+
+    socket.on("admin_user_in_support", async (params) => {
+        const {user_id} = params;
+        await connectionService.updateAdminId(user_id, socket.id);
+
+        const allConnectionsWithoutAdmin = await connectionService.findAllWithoutAdmin();
+
+        io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+    })
+
+
+
 });
